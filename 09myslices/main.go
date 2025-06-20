66b97eb7d652cf3c9efeaab8e50dfeadeb6311e0
robert1745 <0745naveen@gmail.com>
2025-06-20 17:16:54 +0000

    learned till slices

---
+++ 09myslices/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to learning about slices")
+	var fruitList = []string{"Apple", "Tomato", "Peach"}
+	fmt.Printf("Type of fruitlist is %T\n", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Banana")
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[1:3])
+	fmt.Println(fruitList)
+
+}
